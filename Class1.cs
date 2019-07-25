f1decaa2de552d93a40e5254dc8d7cbf902547a3
admin <meerm@cybage.com>
2019-07-25 15:36:18 +0530

    Added code for multiplication

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_2
+{
+    public class Class1
+    {
+        public double Addition(int no1, int no2)
+        {
+            return no1 + no2;
+        }
+
+        public int Multip(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
