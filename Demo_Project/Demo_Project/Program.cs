8ff5cda85e876f8f5116ee52915cabde3042a3a9
sudhanshush <sudhanshush@SUDHANSHUSH-W81>
2019-07-31 17:14:51 +0530

    add changes

---
+++ Demo_Project/Demo_Project/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Demo_Project
+{
+    public class Program
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            int Number1, Number2;
+            Console.WriteLine("please enter the Number1");
+            Number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter the Number2");
+            Number2 = Convert.ToInt32(Console.ReadLine());
+            Program p = new Program();
+            int Result;
+            Result = p.add(Number1, Number2);
+            Console.WriteLine("Sum of two Numbers:" + Result.ToString());
+            Console.ReadLine();
+        }
+    }
+}
