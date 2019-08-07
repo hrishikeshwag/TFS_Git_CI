1ababfba48155a3bbb5a0a2628b4a1a434a5d016
sudhanshush <sudhanshush@SUDHANSHUSH-W81>
2019-08-07 17:57:51 +0530

    first commit

--- Sintu/Program.cs
+++ Sintu/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace Sintu
 {
-    class Program
+    public class Program
     {
+        public int mul(int a, int b)
+        {
+            return a*b;
+        }
         static void Main(string[] args)
         {
+            int Number1, Number2;
+            Console.WriteLine("please enter the Number1");
+            Number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter the Number2");
+            Number2 = Convert.ToInt32(Console.ReadLine());
+            Program p = new Program();
+            int Result;
+            Result = p.mul(Number1, Number2);
+            Console.WriteLine("Sum of two Numbers:" + Result.ToString());
+            Console.ReadLine();
         }
     }
 }
